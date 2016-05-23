9dc33a3fafc9ae987d847e1bdbadb8c97220c757
David Mather <davidmather@live.ie>
2016-05-23 23:23:12 +0100

    0

--- app/scripts/ninjatest.js
+++ app/scripts/ninjatest.js
@@ -9,7 +9,8 @@
             game.load.image('sky', 'images/phaserexamples/sky.png');
             game.load.image('ground', 'images/phaserexamples/platform.png');
             game.load.image('star', 'images/phaserexamples/star.png');
-            game.load.spritesheet('dude', 'images/ninja/spritesheet/parts/ResizedRunAndIdle.png', 45.5, 54);
+            //game.load.spritesheet('dude', 'images/ninja/spritesheet/parts/ResizedRunAndIdle.png', 45.5, 54);
+            game.load.atlasJSONHash('dude', 'images/ninjagirl/ninjagirl.png', 'images/ninjagirl/ninjagirl.json');
          
            
         }
@@ -61,10 +62,14 @@
 
             //  Our two animations, walking left and right.
             //player.animations.add('stop', [20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39], 10, true);
-           player.animations.add('left', [10, 11, 12, 13, 14, 15, 16, 17, 18, 19], 10, true);
-           player.animations.add('right', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, true);      
+           player.animations.add('attack', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, true);
+           player.animations.add('idle', [40, 41, 42, 43, 44, 45, 46, 47, 48, 49], 10, true);
+           player.animations.add('jump', [70, 71, 72, 73, 74, 75, 76, 77, 78, 79], 5, true);    
+           player.animations.add('right', [81, 82, 83, 84, 85, 86, 87, 88, 89, 90], 10, true); 
               
-            
+             // Set Anchor to the center of your sprite
+
+                player.anchor.setTo(.5,.5);
             createstars();
             scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
         }
@@ -76,33 +81,53 @@
               //  Reset the players velocity (movement)
             player.body.velocity.x = 0;
             cursors = game.input.keyboard.createCursorKeys();
+
             if (cursors.left.isDown)
             {
                 //  Move to the left
                 player.body.velocity.x = -150;
+                
+               if(!player.body.touching.down){
+                player.animations.play('jump');
+                }else{player.animations.play('right');}
+                
 
-                player.animations.play('left');
+                // Invert scale.x to flip left/right
+
+                player.scale.x = -1;
+
+            
+
+                
+                
             }
             else if (cursors.right.isDown)
             {
                 //  Move to the right
                 player.body.velocity.x = 150;
+                
+                if(!player.body.touching.down){
+                player.animations.play('jump');
+                }else{player.animations.play('right');}
+             
 
-                player.animations.play('right');
+                // Invert scale.x to flip left/right
+
+                player.scale.x = 1;
             }
+
             else
             {
                 //  Stand still
-                player.animations.stop();
+                player.animations.play('idle');
                 
-                //player.animations.play('stop');
                 
-                player.frame = 24;
             }
 
             //  Allow the player to jump if they are touching the ground.
             if (cursors.up.isDown && player.body.touching.down)
             {
+                player.animations.play('jump');
                 player.body.velocity.y = -350;
             }
        }
